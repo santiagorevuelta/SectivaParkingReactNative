03790de6c0347e88386089da7e64e2e7c13607da
Santi.rev <santiagorevuel@gmail.com>
2023-05-20 06:55:16 -0500

    App

--- src/helpers/inputsValidator.js
+++ src/helpers/inputsValidator.js
@@ -1,50 +1,48 @@
 export function nameValidator(name) {
-  if (!name) return 'El nombre no puede estar vacío';
+  if (!name) {
+    return 'El nombre no puede estar vacío';
+  }
   return '';
 }
 
 export function lastNameValidator(name) {
-  if (!name) return 'El apellido no puede estar vacío.';
+  if (!name) {
+    return 'El apellido no puede estar vacío.';
+  }
   return '';
 }
 
 export function ccValidator(name) {
-  if (!name) return 'La identificacion puede estar vacío.';
+  if (!name) {
+    return 'La identificacion no puede estar vacío.';
+  }
   return '';
 }
-export function numeroDestinoValidator(name) {
-  if (!name) return 'El numero no puede estar vacío.';
-  return '';
-}
-export function cantidadValidator(name) {
-  if (!name) return 'La cantidad no puede estar vacía.';
-  return '';
-}
-export function referenciaValidator(name) {
-  if (!name) return 'La referencia no puede estar vacía.';
-  return '';
-}
+
 export function emailValidator(email) {
   const re = /\S+@\S+\.\S+/;
-  if (!email) return 'El correo electrónico no puede estar vacío.';
-  if (!re.test(email))
+  if (!email) {
+    return 'El correo electrónico no puede estar vacío.';
+  }
+  if (!re.test(email)) {
     return '¡Ooops! Necesitamos una dirección de correo electrónico valida.';
+  }
   return '';
 }
 
 export function passwordValidator(password) {
-  if (!password) return 'La contraseña no puede estar vacía.';
-  if (password.length < 5)
+  if (!password) {
+    return 'La contraseña no puede estar vacía.';
+  }
+  if (password.length < 5) {
     return 'La contraseña debe tener al menos 5 caracteres.';
+  }
   return '';
 }
 
 export function isNumeric(numero) {
   const reg = new RegExp(/^[0-9]+$/);
-  if (reg.test(numero) || numero === '') {
-    return true;
-  }
-  return false;
+  return reg.test(numero) || numero === '';
 }
 
 export const primeraLetra = str => {
